71973e131075552d438682d354680462c4baa846
Mia Starr <starrmiamks@gmail.com>
2021-03-04 20:16:12 -0500

    adding delete functionality

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,55 +1,64 @@
 
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-    Container, CardImg, CardBody,
-    CardTitle, Button, Col, Row
+  Container, CardImg, CardBody,
+  CardTitle, Button, Col, Row
 } from 'reactstrap';
 
 const Favorites = () => {
-    const [favorites, setFavorites] = useState([])
+  const [favorites, setFavorites] = useState([])
 
-    useEffect(()=>{
+  useEffect(() => {
     fetch('http://localhost:3000/favorites/mine', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: localStorage.getItem('token'),
-            }
-        }) 
-        .then(res => (res.json()))
-        .then(json => {
-            setFavorites(json)
-            // console.log(json, "this is a console log");
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('token'),
+      }
     })
+      .then(res => (res.json()))
+      .then(json => {
+        setFavorites(json)
+      })
+  }, [])
 
-        // setFavorites()
-    },[])
+  const deleteFav = (e, id) => {
+    e.preventDefault()
+    fetch(`http://localhost:3000/favorites/delete/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('token'),
+      }
+    }).then(()=>{
+      let tmp = favorites.filter(favorite => favorite.id !== id)
+      setFavorites(tmp)
+    })
+  }
 
+  return (
+    <div className="card">
+      <Container>
+        <Row>
+          {favorites.map(favorite => {
+            return (
+              <Col key={favorite.id} lg="4" sm="6" xs="12" className="grid-column">
+                <div className="resultsCard"
+                >
+                  <CardImg top width="25%" src={favorite.imageURL} alt="Recipe Image" />
+                  <CardBody >
+                    <CardTitle tag="h5">{favorite.title}</CardTitle>
+                    <Button onClick={e => deleteFav(e, favorite.id)}>Delete</Button>
+                  </CardBody>
+                </div>
 
-    return (
-        <div className="card">
-           <Container> 
-                <Row>
-            {favorites.map(favorite => {
-                return(
-                    <Col key={favorite.id} lg="4" sm="6" xs="12" className="grid-column">
-                        <div className="resultsCard" 
-                        // style={{width: "25%", height: "60%"}}
-                        >
-                            <CardImg top width="25%" src={favorite.imageURL} alt="Recipe Image" />
-                            <CardBody >
-                                <CardTitle tag="h5">{favorite.title}</CardTitle>
-                                <Button>Delete</Button>
-                            </CardBody>
-                        </div>
-                       
-                    </Col>
-                )
-            })}
-                </Row>
-            </Container>
-        </div>
-    )
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
+    </div>
+  )
 }
 
- export default Favorites;
+export default Favorites;
